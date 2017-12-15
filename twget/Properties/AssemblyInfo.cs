2103530fad95d6d8122fb2b0f32808b074793b20
cogorou <cogorou@gmail.com>
2017-12-15 17:16:40 +0900

    modified

--- twget/Properties/AssemblyInfo.cs
+++ twget/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("twget")]
-[assembly: AssemblyDescription("Tool to collect tweet from Twitter")]
+[assembly: AssemblyDescription("Tool to collect Tweets from Twitter")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Eggs Imaging Laboratory")]
 [assembly: AssemblyProduct("twget")]
